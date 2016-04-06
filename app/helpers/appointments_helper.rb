9e02dd8e34fb2a042af32afcc14706e3cc63768b
Nick Stoneman <nick.stoneman@gmail.com>
2016-04-06 16:27:35 -0400

    created a an appointment helper method to show if an appointment has been booked or not

--- app/helpers/appointments_helper.rb
+++ app/helpers/appointments_helper.rb
@@ -4,4 +4,12 @@ module AppointmentsHelper
     boolean ? 'Yes' : 'No'
   end
 
+  def booked_or_not(boolean)
+  	if boolean == true
+  		"Sorry, you're to late. This appointment/reservation has been filled."
+  	else
+  		"This appointment/reservation is still available."
+  end
+end
+
 end
